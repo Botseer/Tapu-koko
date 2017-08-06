20aa4fd761c65da4c6549955834c3c6875b8545c
Botseer <homeu99@gmail.com>
2017-08-06 15:40:29 +0545

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.login(process.env.TOKEN);
+
+client.on('ready',() => {
+	console.log('I am Online');
+	client.user.setGame("Various hacks in this server.");	
+});
+
+client.on('guildMemberAdd', member => {
+  member.guild.channels.get("339785028055597058").send(`Welcome ${member.user.tag}.\nWe hope you will have a great time here`);
+});
+
+client.on('guildMemberRemove', member => {
+  member.guild.channels.get("339785028055597058").send(`${member.user.tag} jsut left ${member.guild.name}`);
+});
+
+process.on("unhandledRejection", err => {
+    console.log(err.stack);
+});
+ client.on('error', (e) => console.error(e));
+ client.on('warn', (e) => console.warn(e));
+ client.on('debug', (e) => console.info(e));
