f5eb06cbab64873e99df7f3ae6a392d3342d84b0
The_Learner <bsantosh909@gmail.com>
2017-08-17 13:00:06 +0545

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ client.on('ready',() => {
 });
 
 client.on('guildMemberAdd', member => {
-  member.guild.channels.get("339785028055597058").send(`Welcome ${member}, We hope you will have a great time here`);
+  if (member.guild.id === "336176652776243200") return member.guild.channels.get("344851615448301568").send(`Welcome ${member}, We hope you will have a great time here at **${member.guild.name}**`);
+  member.guild.channels.get("339785028055597058").send(`Welcome ${member}, We hope you will have a great time here at **${member.guild.name}**`);
 });
 
 client.on('guildMemberRemove', member => {
+  if (member.guild.id === "336176652776243200") return member.guild.channels.get("344851615448301568").send(`**${member.user.tag}** just left **${member.guild.name}**`);
   member.guild.channels.get("339785028055597058").send(`**${member.user.tag}** just left **${member.guild.name}**`);
 });
 
